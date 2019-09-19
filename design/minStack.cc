84926c8d61054620bfd68b6109e81529541dd5d7
wangyy <1285145078@qq.com>
2019-09-19 16:25:13 +0800

    155th：使用一个栈，栈顶保存最小和次小

--- design/minStack.cc
+++ design/minStack.cc
@@ -1,28 +1,76 @@
 #include<iostream>
+#include<stack>
 
 using namespace std;
 class MinStack {
 public:
     /** initialize your data structure here. */
+    /* two stacks
     MinStack() {
         
     }
     
     void push(int x) {
-        
+        s1.push(x);
+        if (s2.empty() || x <= s2.top())
+        {
+            s2.push(x);
+        }
     }
     
     void pop() {
-        
+        if (s1.top() == s2.top())
+        {
+            s2.pop();
+        }
+        s1.pop();
     }
     
     int top() {
-        
+        return s1.top();
     }
     
     int getMin() {
-        
+        return s2.top();
     }
+private:
+    stack<int> s1, s2;
+    */
+    // one stack
+    /** initialize your data structure here. */
+    MinStack() {
+        min_val = INT64_MAX;
+    }
+    
+    void push(int x) {
+        if (x <= min_val)
+        {
+            st.push(min_val);
+            min_val = x;
+        }
+        st.push(x);
+    }
+    
+    void pop() {
+        int x = st.top();
+        st.pop();
+        if (min_val == x)
+        {
+            min_val = st.top();
+            st.pop();
+        }
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return min_val;
+    }
+private:
+    stack<int> st;
+    int min_val;
 };
 
 /**
@@ -36,7 +84,8 @@ public:
 int main()
 {
     MinStack* obj = new MinStack();
-    obj->push(x);
+    obj->push(1);
+    obj->push(3);
     obj->pop();
     cout << obj->top() << endl;
     cout << obj->getMin();
